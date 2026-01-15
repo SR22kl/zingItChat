a8130604f4d39df4d80080ee948b3e4695e1a698
Sumit Rathod <sbrathod22kl@gmail.com>
2026-01-15 14:25:00 +0530

    loader changes made

--- client/src/utils/Loader.jsx
+++ client/src/utils/Loader.jsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
-import { FaWhatsapp } from "react-icons/fa";
+import { RiChatSmileAiLine } from "react-icons/ri";
 
 export default function Loader({ progress = 0 }) {
   return (
-    <div className="fixed inset-0 bg-linear-to-br from-green-400 to-blue-500 flex flex-col items-center justify-center z-50">
+    <div className="fixed inset-0 bg-linear-to-br from-purple-400 to-violet-500 flex flex-col items-center justify-center z-50">
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
@@ -15,7 +15,7 @@ export default function Loader({ progress = 0 }) {
         }}
         className="w-24 h-24 bg-white rounded-full flex items-center justify-center mb-8"
       >
-        <FaWhatsapp className="w-16 h-16 text-green-500" />
+        <RiChatSmileAiLine className="w-16 h-16 text-purple-500" />
       </motion.div>
       <div className="w-64 bg-white bg-opacity-30 rounded-full h-2 mb-4">
         <motion.div
